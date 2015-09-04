310bccb4f8e1b1105b5d886b7c6ce97192b3f4a3
António Nuno Monteiro <anmonteiro@gmail.com>
2015-09-04 14:09:31 +0100

    use figwheel
    involves changing paths because figwheel currently only serves from the
    resources dir

--- project.clj
+++ project.clj
@@ -9,35 +9,35 @@
                  [org.omcljs/om "0.9.0"]]
 
   :plugins [[lein-cljsbuild "1.1.0"]
+            [lein-figwheel "0.3.8"]
             [com.cemerick/clojurescript.test "0.3.3"]]
   :source-paths ["src"]
-  :clean-targets [[:cljsbuild :builds 0 :compiler :output-dir]
+  :clean-targets ^{:protect false} [[:cljsbuild :builds 0 :compiler :output-dir]
                   [:cljsbuild :builds 0 :compiler :output-to]
                   "target"]
-  :cljsbuild {
-    :builds [{:id "dev"
-              :source-paths ["src"]
-              :compiler {
-                :main deathrow.core
-                :output-to "public/js/deathrow.js"
-                :output-dir "out"
-                :optimizations :none
-                :source-map true
-                }}
-                {:id "prod"
-                 :source-paths ["src"]
-                 :compiler {
-                  :main deathrow.core
-                  :output-to "public/js/deathrow.js"
-                  :optimizations :advanced
-                  :pretty-print false
-                }}
-                {:id "test"
-                :source-paths ["test"]
-                :compiler {
-                :output-to "target/test/deathrow.test.js"
-                :output-dir "target/test"
-                :optimizations :none
-                }}]
-  :test-commands {"unit-tests" ["node" :node-runner
-                                  "target/test/deathrow.test.js"]}})
+  :figwheel {:css-dirs ["resources/public/css"]}
+  :cljsbuild {:builds [{:id "dev"
+                        :source-paths ["src"]
+                        :figwheel true
+                        :compiler {:main deathrow.core
+                                   :asset-path "js/out"
+                                   :output-to "resources/public/js/deathrow.js"
+                                   :output-dir "resources/public/js/out"
+                                   :optimizations :none
+                                   :source-map true}}
+                       {:id "prod"
+                        :source-paths ["src"]
+                        :compiler {:main deathrow.core
+                                   :output-to "resources/public/js/deathrow.js"
+                                   :elide-asserts true
+                                   :optimizations :advanced
+                                   :pretty-print false}}
+                       {:id "test"
+                        :source-paths ["test"]
+                        :compiler {:output-to "target/test/deathrow.test.js"
+                                   :output-dir "target/test"
+                                   :optimizations :none}}]
+              :test-commands {"unit-tests"
+                              ["node" :node-runner "target/test/deathrow.test.js"]}}
+  :aliases {"auto-dev" ["do" ["clean"] ["cljsbuild" "auto" "dev"]]
+            "publish" ["do" ["clean"] ["cljsbuild" "once" "prod"]]})
